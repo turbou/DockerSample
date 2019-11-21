40c77432d3d13d32914e5f78945f18f815668cf4
turbou <turbou@i.softbank.jp>
2019-11-22 02:22:19 +0900

    update.

--- integration/mantis/html/plugins/ContrastSecurity/ContrastSecurity.php
+++ integration/mantis/html/plugins/ContrastSecurity/ContrastSecurity.php
@@ -4,9 +4,6 @@ require_api('authentication_api.php');
 require_api('user_api.php');
 require_api('api_token_api.php');
 
-$t_restlocal_dir = __DIR__ . '/core/';
-require_once( $t_restlocal_dir . 'MyAuthMiddleware.php' );
-
 class ContrastSecurityPlugin extends MantisPlugin {
     public function register() {
         $this->name = plugin_lang_get("title");
@@ -85,9 +82,9 @@ class ContrastSecurityPlugin extends MantisPlugin {
                     echo "Hello, " . $args['name'] . "!!\r\n";
                 }); 
                 $t_app->get( '/{key}/test', [$t_plugin, 'rest_auth_test']);
-                $t_app->post( '/{key}/issue', [$t_plugin, 'rest_issue_add']);
+                $t_app->post( '/{key}/issues', [$t_plugin, 'rest_issue_add']);
             }
-        )->add(new MyAuthMiddleware);
+        );
     }
     /**
      * A method that does the work to handle getting an issue via REST API.
@@ -97,60 +94,10 @@ class ContrastSecurityPlugin extends MantisPlugin {
      * @param array $p_args Arguments
      * @return \Slim\Http\Response The augmented response.
      */
-    #function rest_issue_get( \Slim\Http\Request $p_request, \Slim\Http\Response $p_response, array $p_args ) {
-    function rest_auth_test($p_request, $p_response, array $p_args) {
+    function rest_auth_test(\Slim\Http\Request $p_request, \Slim\Http\Response $p_response, array $p_args) {
         error_log('rest_auth_test');
-    	$t_key = isset($p_args['key']) ? $p_args['key'] : $p_request->getParam('key');
-        error_log($t_key);
+        $t_key = isset($p_args['key']) ? $p_args['key'] : $p_request->getParam('key');
         return $p_response->withHeader(HTTP_STATUS_SUCCESS, "Success");
-
-#    	if( !is_blank( $t_issue_id ) ) {
-#    		# Get Issue By Id
-#    
-#    		# Username and password below are ignored, since middleware already done the auth.
-#    		$t_issue = mc_issue_get( /* username */ '', /* password */ '', $t_issue_id );
-#    		ApiObjectFactory::throwIfFault( $t_issue );
-#    
-#    		$t_result = array( 'issues' => array( $t_issue ) );
-#    	} else {
-#    		$t_page_number = $p_request->getParam( 'page', 1 );
-#    		$t_page_size = $p_request->getParam( 'page_size', 50 );
-#    
-#    		# Get a set of issues
-#    		$t_project_id = (int)$p_request->getParam( 'project_id', ALL_PROJECTS );
-#    		if( $t_project_id != ALL_PROJECTS && !project_exists( $t_project_id ) ) {
-#    			$t_result = null;
-#    			$t_message = "Project '$t_project_id' doesn't exist";
-#    			$p_response = $p_response->withStatus( HTTP_STATUS_NOT_FOUND, $t_message );
-#    		} else {
-#    			$t_filter_id = trim( $p_request->getParam( 'filter_id', '' ) );
-#    
-#    			if( !empty( $t_filter_id ) ) {
-#    				$t_issues = mc_filter_get_issues(
-#    					'', '', $t_project_id, $t_filter_id, $t_page_number, $t_page_size );
-#    			} else {
-#    				$t_issues = mc_filter_get_issues(
-#    					'', '', $t_project_id, FILTER_STANDARD_ANY, $t_page_number, $t_page_size );
-#    			}
-#    
-#    			$t_result = array( 'issues' => $t_issues );
-#    		}
-#    	}
-#    
-#    	$t_etag = mc_issue_hash( $t_issue_id, $t_result );
-#    	if( $p_request->hasHeader( HEADER_IF_NONE_MATCH ) ) {
-#    		$t_match_etag = $p_request->getHeaderLine( HEADER_IF_NONE_MATCH );
-#    		if( $t_etag == $t_match_etag ) {
-#    			return $p_response->withStatus( HTTP_STATUS_NOT_MODIFIED, 'Not Modified' )
-#    				->withHeader( HEADER_ETAG, $t_etag );
-#    		}
-#    	}
-#    
-#    	if( $t_result !== null ) {
-#    		$p_response = $p_response->withStatus( HTTP_STATUS_SUCCESS )->withJson( $t_result );
-#    	}
-#
-#    	return $p_response->withHeader( HEADER_ETAG, $t_etag );
     }
     
     /**
@@ -161,23 +108,19 @@ class ContrastSecurityPlugin extends MantisPlugin {
      * @param array $p_args Arguments
      * @return \Slim\Http\Response The augmented response.
      */
-    function rest_issue_add( \Slim\Http\Request $p_request, \Slim\Http\Response $p_response, array $p_args ) {
-            $t_issue = $p_request->getParsedBody();
-    
-            if( isset( $t_issue['files'] ) ) {
-                    $t_issue['files'] = files_base64_to_temp( $t_issue['files'] );
-            }
-    
-            $t_data = array( 'payload' => array( 'issue' => $t_issue ) );
-            $t_command = new IssueAddCommand( $t_data );
-            $t_result = $t_command->execute();
-            $t_issue_id = (int)$t_result['issue_id'];
-    
-            $t_created_issue = mc_issue_get( /* username */ '', /* password */ '', $t_issue_id );
-    
-            return $p_response->withStatus( HTTP_STATUS_CREATED, "Issue Created with id $t_issue_id" )->
-                    withJson( array( 'issue' => $t_created_issue ) );
+    function rest_issue_add(\Slim\Http\Request $p_request, \Slim\Http\Response $p_response, array $p_args) {
+        $t_issue = $p_request->getParsedBody();
+
+        $t_data = array('payload' => array('issue' => $t_issue));
+        error_log(var_dump($t_data));
+#        $t_command = new IssueAddCommand( $t_data );
+#        $t_result = $t_command->execute();
+#        $t_issue_id = (int)$t_result['issue_id'];
+#
+#        $t_created_issue = mc_issue_get( /* username */ '', /* password */ '', $t_issue_id );
+#
+#        return $p_response->withStatus( HTTP_STATUS_CREATED, "Issue Created with id $t_issue_id" )->
+#           withJson( array( 'issue' => $t_created_issue ) );
     }
 }
-
 
