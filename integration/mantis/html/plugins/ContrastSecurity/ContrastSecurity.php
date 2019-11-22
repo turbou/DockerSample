d88da329a948dca731ecf2134b6dcfb3537f2545
turbou <turbou@i.softbank.jp>
2019-11-23 03:15:36 +0900

    update.

--- integration/mantis/html/plugins/ContrastSecurity/ContrastSecurity.php
+++ integration/mantis/html/plugins/ContrastSecurity/ContrastSecurity.php
@@ -81,12 +81,10 @@ class ContrastSecurityPlugin extends MantisPlugin {
         $t_app = $p_event_args['app'];
         $t_app->add(function($req, $res, $next) {
             $path = $req->getUri()->getPath();
-            #error_log('path: ' . $path);
             $is_exist_token = preg_match('/^plugins\/ContrastSecurity\/services\/(\w+)/', $path, $match);
             if(!$is_exist_token) {
                 return $next($req, $res);
             }
-            #error_log('key: ' . $match[1]);
             $token = $match[1];
             $t_user_id = api_token_get_user($token);
             if( $t_user_id !== false ) {
@@ -102,7 +100,6 @@ class ContrastSecurityPlugin extends MantisPlugin {
             if(mci_check_login($t_username, $t_password) === false) {
                 return $res->withStatus(HTTP_STATUS_FORBIDDEN, 'Access denied');
             }
-            #$res->getBody()->write($t_username . " middleware #2\n");
             return $next($req, $res);
         });
         $t_plugin = $this;
@@ -121,17 +118,11 @@ class ContrastSecurityPlugin extends MantisPlugin {
      */
     function issue_add(\Slim\Http\Request $p_request, \Slim\Http\Response $p_response, array $p_args) {
         $contentType = $p_request->getContentType();
-        #error_log($contentType);
-        #$t_issue = $p_request->getParsedBody();
         $json_data = $p_request->getBody();
-        #error_log('json_data: ' . $json_data);
         $t_issue = json_decode($json_data, true);
-        #error_log('t_issue: ' . var_dump($t_issue));
         if ($t_issue["applicationName"] == "ContrastTestApplication") {
             return $p_response->withHeader(HTTP_STATUS_SUCCESS, "Success");
         }
-        #error_log(var_dump($t_issue));
-        #error_log($t_issue['description']);
         $is_exist = preg_match('/index.html#\/(.+)\/applications\/(.+)\/vulns\/(.+)\) was found in/', $t_issue['description'], $match);
         if ($is_exist) {
             plugin_push_current('ContrastSecurity');
@@ -141,7 +132,6 @@ class ContrastSecurityPlugin extends MantisPlugin {
             $vul_id = $match[3];
             # /Contrast/api/ng/[ORG_ID]/traces/[APP_ID]/trace/[VUL_ID]
             $url = sprintf('%s/api/ng/%s/traces/%s/trace/%s', $teamserver_url, $org_id, $app_id, $vul_id);
-            #error_log($url);
             $get_data = callAPI('GET', $url, false);
             $vuln_json = json_decode($get_data, true);
             $summary = $vuln_json["trace"]["title"];
@@ -156,26 +146,18 @@ class ContrastSecurityPlugin extends MantisPlugin {
             $app_id_id = custom_field_get_id_from_name(VUL);
             array_push($custom_fields, array('field' => array('id' => $vul_id_id, 'name' => VUL), 'value' => $vul_id));
             $t_issue['custom_fields'] = $custom_fields;
-
-            #error_log('custom_fields add: ' . var_dump($t_issue));
             plugin_pop_current('ContrastSecurity');
         } else {
-            error_log('nonmatch');
+            return $p_response->withHeader(HTTP_STATUS_SUCCESS, "Test URL Success");
         }
 
         $t_data = array('payload' => array('issue' => $t_issue));
         $t_command = new IssueAddCommand($t_data);
         $t_result = $t_command->execute();
-        #error_log(var_dump($t_result));
-        #$t_issue_id = (int)$t_result['issue_id'];
-        #$t_created_issue = mc_issue_get( /* username */ '', /* password */ '', $t_issue_id );
-        #return $p_response->withStatus( HTTP_STATUS_CREATED, "Issue Created with id $t_issue_id" )->withJson( array( 'issue' => $t_created_issue ) );
         return $p_response->withHeader(HTTP_STATUS_SUCCESS, "Success");
     }
 
     function bug_update($p_event_name, $t_existing_bug, $t_updated_bug) {
-        error_log('event_name: ' . $p_event_name);
-        error_log('status: ' . $t_existing_bug->status . ' -> ' . $t_updated_bug->status);
         plugin_push_current('ContrastSecurity');
         $teamserver_url = plugin_config_get('teamserver_url');
 
@@ -190,7 +172,6 @@ class ContrastSecurityPlugin extends MantisPlugin {
         $url = sprintf('%s/api/ng/%s/orgtraces/mark', $teamserver_url, $org_id);
         $t_data = array('traces' => array($vul_id), 'status' => 'Confirmed', 'note' => 'from mantisbt.');
         $put_result = callAPI('PUT', $url, json_encode($t_data));
-        error_log($put_result);
         $result = json_decode($put_result, true);
         plugin_pop_current('ContrastSecurity');
     }
