84edf4c48bc8b7ec49fdb72d7c50693fd558b197
turbou <turbou@i.softbank.jp>
2019-12-01 12:09:58 +0900

    update.

--- integration/mantis/html/plugins/ContrastSecurity/ContrastSecurity.php
+++ integration/mantis/html/plugins/ContrastSecurity/ContrastSecurity.php
@@ -231,41 +231,41 @@ class ContrastSecurityPlugin extends MantisPlugin {
 }
 
 function callAPI($method, $url, $data){
-   $curl = curl_init();
-   switch ($method){
-      case "POST":
-         curl_setopt($curl, CURLOPT_POST, 1);
-         if ($data)
-            curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-         break;
-      case "PUT":
-         curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PUT");
-         if ($data)
-            curl_setopt($curl, CURLOPT_POSTFIELDS, $data);			 					
-         break;
-      default:
-         if ($data)
-            $url = sprintf("%s?%s", $url, http_build_query($data));
-   }
-
-   // OPTIONS:
-   $api_key = plugin_config_get('api_key');
-   $auth_header = plugin_config_get('auth_header');
-   curl_setopt($curl, CURLOPT_URL, $url);
-   curl_setopt($curl, CURLOPT_HTTPHEADER, array(
-      'Authorization: ' . $auth_header,
-      'API-Key: ' . $api_key,
-      'Accept: application/json',
-      'Content-Type: application/json',
-   ));
-   curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-   curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-   curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 10); 
-
-   // EXECUTE:
-   $result = curl_exec($curl);
-   if(!$result){die("Connection Failure");}
-   curl_close($curl);
-   return $result;
+    $curl = curl_init();
+    switch ($method){
+        case "POST":
+            curl_setopt($curl, CURLOPT_POST, 1);
+            if ($data)
+                curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+            break;
+        case "PUT":
+            curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PUT");
+            if ($data)
+                curl_setopt($curl, CURLOPT_POSTFIELDS, $data);			 					
+            break;
+        default:
+            if ($data)
+                $url = sprintf("%s?%s", $url, http_build_query($data));
+    }
+ 
+    // OPTIONS:
+    $api_key = plugin_config_get('api_key');
+    $auth_header = plugin_config_get('auth_header');
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+        'Authorization: ' . $auth_header,
+        'API-Key: ' . $api_key,
+        'Accept: application/json',
+        'Content-Type: application/json',
+    ));
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+    curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 10); 
+ 
+    // EXECUTE:
+    $result = curl_exec($curl);
+    if(!$result){die("Connection Failure");}
+    curl_close($curl);
+    return $result;
 }
 
