300c2514a2a9d07268de7903c0f15b7b7b1a5ed7
turbou <turbou@i.softbank.jp>
2020-02-02 02:49:52 +0900

    update.

--- agent/ruby/rails5_simple/clear.sh
+++ agent/ruby/rails5_simple/clear.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-rm -fr Rakefile app bin config config.ru db lib log package.json public/ storage/ tmp vendor/ .ruby-version .git .gitignore test
+rm -fr Rakefile app bin config config.ru db lib log package.json public/ storage/ tmp vendor/ .ruby-version .git .gitignore test README.md
 git checkout Gemfile
 git checkout Gemfile.lock
 
