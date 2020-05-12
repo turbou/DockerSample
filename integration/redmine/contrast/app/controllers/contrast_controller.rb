3ab7d5adc0ae10df57721bea6b389ee6a5036ba4
root <root@ip-172-31-47-104.ap-northeast-1.compute.internal>
2020-05-13 02:07:35 +0900

    update.

--- integration/redmine/contrast/app/controllers/contrast_controller.rb
+++ integration/redmine/contrast/app/controllers/contrast_controller.rb
@@ -4,18 +4,56 @@ class ContrastController < ApplicationController
   accept_api_auth :vote
 
   def vote
-    teamserver_url = Setting.plugin_contrast['teamserver_url']
     #logger.info(request.body.read)
     t_issue = JSON.parse(request.body.read)
-    logger.info(t_issue)
-    is_vul = t_issue['description'].match('/index.html#\/(.+)\/applications\/(.+)\/vulns\/(.+)\) was found in/')
-    is_lib = t_issue['description'].match('/.+ was found in ([^(]+) \(.+index.html#\/(.+)\/.+\/(.+)\/([^)]+)\),.+\/applications\/([^)]+)\)./')
+    #logger.info(t_issue['description'])
+    vul_pattern = /index.html#\/(.+)\/applications\/(.+)\/vulns\/(.+)\) was found in/
+    lib_pattern = /.+ was found in ([^(]+) \(.+index.html#\/(.+)\/.+\/(.+)\/([^)]+)\),.+\/applications\/([^)]+)\)./
+    is_vul = t_issue['description'].match(vul_pattern)
+    is_lib = t_issue['description'].match(lib_pattern)
     if is_vul then
-      if Setting.plugin_contrast['vul_issues'] then
+      if not Setting.plugin_contrast['vul_issues'] then
         return render plain: 'Vul Skip'
       end
+      org_id = is_vul[1];
+      app_id = is_vul[2];
+      vul_id = is_vul[3];
+      lib_id = "";
+      # /Contrast/api/ng/[ORG_ID]/traces/[APP_ID]/trace/[VUL_ID]
+      teamserver_url = Setting.plugin_contrast['teamserver_url']
+      url = sprintf('%s/api/ng/%s/traces/%s/trace/%s', teamserver_url, org_id, app_id, vul_id);
+      logger.info(url)
+      get_data = callAPI('GET', url, false)
+      vuln_json = JSON.parse(get_data)
+      summary = vuln_json['trace']['title']
+      story_url = ''
+      howtofix_url = ''
+      self_url = ''
+      vuln_json['trace']['links'].each do |c_link|
+        if c_link['rel'] == 'self' then
+          self_url = c_link['href']
+        end
+        if c_link['rel'] == 'story' then
+          story_url = c_link['href']
+        end
+        if c_link['rel'] == 'recommendation' then
+          howtofix_url = c_link['href']
+        end
+      end
+      logger.info(summary)
+      logger.info(story_url)
+      logger.info(howtofix_url)
+      logger.info(self_url)
+      # Story
+      get_story_data = callAPI('GET', story_url, false)
+      story_json = JSON.parse(get_story_data)
+      story = story_json['story']['risk']['text'];
+      # How to fix
+      get_howtofix_data = callAPI('GET', howtofix_url, false);
+      howtofix_json = JSON.parse(get_howtofix_data)
+      howtofix = howtofix_json['recommendation']['text'];
     elsif is_lib then
-      if Setting.plugin_contrast['lib_issues'] then
+      if not Setting.plugin_contrast['lib_issues'] then
         return render plain: 'Lib Skip'
       end
     else
@@ -23,6 +61,17 @@ class ContrastController < ApplicationController
     end
     personal = {'name' => 'Yamada', 'old' => 28}
     render :json => personal
+  end
+
+  def callAPI(method, url, data)
+    uri = URI.parse(url)
+    http = Net::HTTP.new(uri.host, uri.port)
+    req = Net::HTTP::Get.new(uri.request_uri)
+    req["Authorization"] = Setting.plugin_contrast['auth_header']
+    req["API-Key"] = Setting.plugin_contrast['api_key']
+    req['Content-Type'] = req['Accept'] = 'application/json'
+    res = http.request(req)
+    return res.body
   end
 end
 
