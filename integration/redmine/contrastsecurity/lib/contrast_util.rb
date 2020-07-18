239eb16f6ff13a5c49435abed8dc06b5e6257827
turbou <turbou@i.softbank.jp>
2020-07-18 02:05:05 +0000

    update.

--- integration/redmine/contrastsecurity/lib/contrast_util.rb
+++ integration/redmine/contrastsecurity/lib/contrast_util.rb
@@ -18,6 +18,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+
 module ContrastUtil
   def self.get_priority_by_severity(severity)
     case severity
