f15fc19a992b6a033e3cea40cb5c348745acc1c0
turbou <turbou@i.softbank.jp>
2020-07-31 16:37:13 +0000

    update.

--- integration/redmine/import_shell/import.sh
+++ integration/redmine/import_shell/import.sh
@@ -21,7 +21,7 @@ API_URL="${BASEURL}api/ng/${ORG_ID}"
 APP_NAME=$CONTRAST_APP_NAME
 RM_BASEURL=$REDMINE_BASEURL
 RM_API_KEY=$REDMINE_API_KEY
-RM_PROJ_ID=$REDMINE_PROJECT_ID
+#RM_PROJ_ID=$REDMINE_PROJECT_ID
 
 rm -f ./applications.json
 curl -X GET -sS \
@@ -31,27 +31,26 @@ curl -X GET -sS \
      -H 'Accept: application/json' -J -o applications.json
 
 if [ -s ./applications.json ]; then
-    SUCCESS=`cat ./applications.json | jq -r '.success'`
-    if [ "${SUCCESS}" != "true" ]; then
-        echo "アプリケーション一覧の取得に失敗しました。権限などをご確認ください。"
-        exit 1
-    fi
-else
-    echo "Authorizationヘッダ, APIキー, 組織ID, TeamServerのURLが正しいか、ご確認ください。"
-    echo "接続先URL: " $API_URL
+  SUCCESS=`cat ./applications.json | jq -r '.success'`
+  if [ "${SUCCESS}" != "true" ]; then
+    echo "アプリケーション一覧の取得に失敗しました。権限などをご確認ください。"
     exit 1
+  fi
+else
+  echo "Authorizationヘッダ, APIキー, 組織ID, TeamServerのURLが正しいか、ご確認ください。"
+  echo "接続先URL: " $API_URL
+  exit 1
 fi
 
-APP_ID=""
-while read -r LINE; do
-    APP_ID="${LINE}"
-done < <(cat ./applications.json | jq -r --arg app_name "$APP_NAME" '.applications[] | select(.name==$app_name) | .app_id')
+cat ./applications.json | jq -r --arg app_name "$APP_NAME" '.applications[] | select(.name==$app_name) | {name, app_id}' > application.json
+APP_ID=`cat ./application.json | jq -r '.app_id'`
+CORRECT_APP_NAME=`cat ./application.json | jq -r '.name'`
 
 if [ "$APP_ID" = "" ]; then
-    echo "${APP_NAME} という名前のアプリケーションが見つかりません。"
-    exit 1
+  echo "${APP_NAME} という名前のアプリケーションが見つかりません。"
+  exit 1
 fi
-echo "${APP_NAME} -> ${APP_ID}"
+echo "${CORRECT_APP_NAME} -> ${APP_ID}"
 
 # まずは検知した脆弱性のUUIDリストを取得
 rm -f ./traces_ids.json
@@ -83,54 +82,27 @@ while read -r LINE; do
     echo "ステータス    : ${STATUS}"
     echo "検出日時      : ${FIRST_TIME_SEEN}"
 
-    jq -n --arg subject "${RULE_TITLE}" --arg proj_id "${RM_PROJ_ID}" -f issues.jq > ${LINE}.json
-    curl -X POST -H "Content-Type: application/json" ${RM_BASEURL}issues.json?key=${RM_API_KEY} -d @${LINE}.json
-
-    # Overview
-    rm -f ./story.json
-    curl -X GET -sS \
-         ${API_URL}/traces/${LINE}/story?expand=skip_links \
-         -H "Authorization: ${AUTHORIZATION}" \
-         -H "API-Key: ${API_KEY}" \
-         -H 'Accept: application/json' -J -o story.json
-    RISK=`cat ./story.json | jq -r '.story.risk.text'`
-    echo "概要          :"
-    echo ${RISK}
-
-    # How to Fix
-    rm -f ./howtofix.json
-    curl -X GET -sS \
-         ${API_URL}/traces/${LINE}/recommendation?expand=skip_links \
-         -H "Authorization: ${AUTHORIZATION}" \
-         -H "API-Key: ${API_KEY}" \
-         -H 'Accept: application/json' -J -o howtofix.json
-    HOWTOFIX=`cat ./howtofix.json | jq -r '.recommendation.text'`
-    echo "How to Fix    :"
-    echo ${HOWTOFIX}
-
-    # CWE
-    CWE=`cat ./howtofix.json | jq -r '.cwe'`
-    echo "CWE           : ${CWE}"
-
-    # OWASP
-    OWASP=`cat ./howtofix.json | jq -r '.owasp'`
-    echo "OWASP         : ${OWASP}"
-
-    # コメント履歴
-    NOTE_LENGTH=`cat ./trace.json | jq -r '.trace.notes | length'`
-    if [ $NOTE_LENGTH -gt 0 ]; then
-        echo "コメント      :"
-        while read -r LINE2; do
-            CREATION_UNIX_TIME=`cat ./trace.json | jq -r --arg note_id "$LINE2" '.trace.notes[] | select(.id==$note_id) | .creation'`
-            CREATION_UNIX_TIME=`echo ${CREATION_UNIX_TIME} | cut -c 1-10`
-            CREATION=`date -d @${CREATION_UNIX_TIME} +"%Y/%m/%d %H:%M"`
-            CREATOR=`cat ./trace.json | jq -r --arg note_id "$LINE2" '.trace.notes[] | select(.id==$note_id) | .creator'`
-            NOTE_HTML=`cat ./trace.json | jq -r --arg note_id "$LINE2" '.trace.notes[] | select(.id==$note_id) | .note'`
-            NOTE=`echo ${NOTE_HTML} | recode html..utf8`
-            echo "${CREATION} - ${CREATOR}"
-            echo "${NOTE}"
-        done < <(cat ./trace.json | jq -r '.trace.notes[] | .id')
-    fi
+    jq -n \
+      --arg Title "Contrast Security" \
+      --arg Message "" \
+      --arg Project "contrastsecurity" \
+      --arg Tracker "脆弱性" \
+      --arg ApplicationName "${CORRECT_APP_NAME}" \
+      --arg ApplicationCode "" \
+      --arg VulnerabilityTags "" \
+      --arg ApplicationId "${APP_ID}" \
+      --arg ServerName "" \
+      --arg ServerId "" \
+      --arg OrganizationId "" \
+      --arg Severity "" \
+      --arg Status "" \
+      --arg TraceId "" \
+      --arg VulnerabilityRule "" \
+      --arg Environment "" \
+      --arg EventType "NEW_VULNERABILITY" \
+      -f webhook.jq > ${LINE}.json
+    #curl -X POST -H "Content-Type: application/json" ${RM_BASEURL}contrast/vote?key=${RM_API_KEY} -d @${LINE}.json
+    exit 0
 done < <(cat ./traces_ids.json | jq -r '.traces[]')
 
 exit 0
---
+++ integration/redmine/import_shell/webhook.jq
@@ -0,0 +1,19 @@
+{
+  "summary":$Title,
+  "description":$Message,
+  "project": $Project,
+  "tracker": $Tracker,
+  "application_name": $ApplicationName,
+  "application_code": $ApplicationCode,
+  "vulnerability_tags": $VulnerabilityTags,
+  "application_id": $ApplicationId,
+  "server_name": $ServerName,
+  "server_id": $ServerId,
+  "organization_id": $OrganizationId,
+  "severity": $Severity,
+  "status": $Status,
+  "vulnerability_id": $TraceId,
+  "vulnerability_rule": $VulnerabilityRule,
+  "environment": $Environment,
+  "event_type": $EventType
+}
