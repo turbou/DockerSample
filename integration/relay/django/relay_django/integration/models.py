04ac2106b97de337e3aef4eda6d6f0c3c7170bda
turbou <turbou@i.softbank.jp>
2020-12-16 22:37:52 +0900

    update.

--- integration/relay/django/relay_django/integration/models.py
+++ integration/relay/django/relay_django/integration/models.py
@@ -4,14 +4,14 @@ from django.utils.translation import gettext_lazy as _
 from application.models import Backlog, Gitlab, GoogleChat
 
 class Integration(models.Model):
-    name = models.CharField('Name', max_length=20, unique=True,
+    name = models.CharField(_('Name'), max_length=20, unique=True,
         validators=[RegexValidator(regex='^[A-Za-z0-9_]{4,20}$', message='名前は半角英数字、アンスコ4文字〜10文字です。')],
-        help_text='この名前をTeamServerのPayloadに設定してください。'
+        help_text=_('Set this name to the TeamServer Generic Webhook Payload.')
     )
-    url = models.URLField('TeamServer URL', help_text='e.g. https://app.contrastsecurity.com/Contrast')
-    api_key = models.CharField('API Key', max_length=50, unique=False)
-    username = models.CharField('Username', max_length=20, unique=False, help_text='Login ID (mail address)')
-    service_key = models.CharField('Service Key', max_length=20, unique=False)
+    url = models.URLField(_('TeamServer URL'), help_text=_('e.g. https://app.contrastsecurity.com/Contrast'))
+    api_key = models.CharField(_('API Key'), max_length=50, unique=False)
+    username = models.CharField(_('Username'), max_length=20, unique=False, help_text=_('Login ID (mail address)'))
+    service_key = models.CharField(_('Service Key'), max_length=20, unique=False)
     backlog = models.ForeignKey(Backlog, verbose_name='Backlog', related_name='integrations', related_query_name='integration', on_delete=models.SET_NULL, blank=True, null=True)
     gitlab = models.ForeignKey(Gitlab, verbose_name='Gitlab', related_name='integrations', related_query_name='integration', on_delete=models.SET_NULL, blank=True, null=True)
     googlechat = models.ForeignKey(GoogleChat, verbose_name='GoogleChat', related_name='integrations', related_query_name='integration', on_delete=models.SET_NULL, blank=True, null=True)
