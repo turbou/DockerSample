f33ca9f1a9c1b82b07da1b65cac80c3ed756a4a9
turbou <turbou@i.softbank.jp>
2020-12-17 18:25:28 +0900

    update.

--- integration/relay/django/relay_django/application/models/backlog.py
+++ integration/relay/django/relay_django/application/models/backlog.py
@@ -35,3 +35,54 @@ class Backlog(models.Model):
         verbose_name = _('Backlog')
         verbose_name_plural = _('Backlog List')
 
+class BacklogVul(models.Model):
+    backlog = models.ForeignKey(Backlog, verbose_name=_('Backlog'), related_name='vuls', related_query_name='vul', on_delete=models.PROTECT)
+    contrast_org_id = models.CharField(_('Organization ID'), max_length=36)
+    contrast_app_id = models.CharField(_('Application ID'), max_length=36)
+    contrast_vul_id = models.CharField(_('Vulnerability ID'), max_length=19, blank=True, null=True)
+    backlog_issue_id = models.PositiveSmallIntegerField(_('Issue ID'))
+
+    def __str__(self):
+        if self.id:
+            return '%010d' % (self.id)
+        return ''
+
+    class Meta:
+        verbose_name = _('Backlog Vulnerability')
+        verbose_name_plural = _('Backlog Vulnerabilities')
+
+class BacklogComment(models.Model):
+    vul = models.ForeignKey(BacklogVul, verbose_name=_('Backlog Vulnerability'), related_name='comments', related_query_name='comment', on_delete=models.CASCADE)
+    comment = models.TextField(_('Comment'))
+    creator = models.CharField(_('Creator'), max_length=200)
+    created_at = models.DateTimeField(_('Created'), blank=True, null=True)
+    updated_at = models.DateTimeField(_('Updated'), blank=True, null=True)
+    contrast_note_id = models.CharField(_('Contrast Note ID'), max_length=36, unique=True)
+    backlog_comment_id = models.PositiveSmallIntegerField(_('Backlog Comment ID'))
+
+    def __str__(self):
+        if self.id:
+            return '%010d' % (self.id)
+        return ''
+
+    class Meta:
+        verbose_name = _('Backlog Vulnerability Comment')
+        verbose_name_plural = _('Backlog Vulnerability Comments')
+
+class BacklogLib(models.Model):
+    backlog = models.ForeignKey(Backlog, verbose_name=_('Backlog'), related_name='libs', related_query_name='lib', on_delete=models.PROTECT)
+    contrast_org_id = models.CharField(_('Organization ID'), max_length=36)
+    contrast_app_id = models.CharField(_('Application ID'), max_length=36)
+    contrast_lib_lg = models.CharField(_('Library Language'), max_length=20, blank=True, null=True)
+    contrast_lib_id = models.CharField(_('Library ID'), max_length=40, blank=True, null=True)
+    backlog_issue_id = models.PositiveSmallIntegerField(_('Issue ID'))
+
+    def __str__(self):
+        if self.id:
+            return '%010d' % (self.id)
+        return ''
+
+    class Meta:
+        verbose_name = _('Backlog Library')
+        verbose_name_plural = _('Backlog Libraries')
+
