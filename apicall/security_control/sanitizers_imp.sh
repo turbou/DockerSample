f4e901bd67cf6db496ad10df0453b7f683894118
turbou <turbou@i.softbank.jp>
2022-04-25 02:40:33 +0900

    update.

--- apicall/security_control/sanitizers_imp.sh
+++ apicall/security_control/sanitizers_imp.sh
@@ -4,8 +4,10 @@ usage() {
   cat <<EOF
   Usage: $0 [options]
 
-  -a| --auth      認証情報json
+  -a|--aut h      認証情報json
   -i|--input      一括登録例外json
+  -d|--delete     既存のセキュリティ制御をインポート前にクリア
+  -p|--prefix     インポート前クリアで残すルール名の接頭辞
   -s|--skeleton   jsonスケルトンファイル生成
 EOF
 }
@@ -14,8 +16,10 @@ usage_full() {
   cat <<EOF
   Usage: $0 [options]
 
-  -a| --auth      認証情報json
+  -a|--auth       認証情報json
   -i|--input      一括登録例外json
+  -d|--delete     既存のセキュリティ制御をインポート前にクリア
+  -p|--prefix     インポート前クリアで残すルール名の接頭辞
   -s|--skeleton   jsonスケルトンファイル生成
 
   ルール一覧は以下の通りです。jsonのrulesには右側の値を指定してください。
@@ -74,7 +78,9 @@ EOF
 
 AUTH_JSON=
 INPUT_JSON=
-while getopts aish-: opt; do
+DEL_FLG=0
+PREFIX=
+while getopts aidpsh-: opt; do
   optarg="${!OPTIND}"
   [[ "$opt" = - ]] && opt="-$OPTARG"
   case "-$opt" in
@@ -84,6 +90,13 @@ while getopts aish-: opt; do
       ;;
     -i|--input)
       INPUT_JSON="$optarg"
+      shift
+      ;;
+    -d|--delete)
+      DEL_FLG=1
+      ;;
+    -p|--prefix)
+      PREFIX="$optarg"
       shift
       ;;
     -s|--skeleton)
@@ -119,9 +132,45 @@ API_KEY=`cat ${AUTH_JSON} | jq -r '.API_KEY'`
 AUTHORIZATION=`cat ${AUTH_JSON} | jq -r '.AUTH_HEADER'`
 API_URL="${BASE_URL}/api/ng"
 
+EXISTING_SANITIZERS_JSON="./existing_sanitizers.json"
+
+if [ ${DEL_FLG} -gt 0 ]; then
+  rm -f ${EXISTING_SANITIZERS_JSON}
+  curl -X GET -sS \
+    ${API_URL}/${ORG_ID}/controls \
+    -d expand=skip_links -d quickFilter=ALL \
+    -H "Authorization: ${AUTHORIZATION}" \
+    -H "API-Key: ${API_KEY}" \
+    -H 'Accept: application/json' -J -o ${EXISTING_SANITIZERS_JSON}
+
+  success=`cat ${EXISTING_SANITIZERS_JSON} | jq -r .success`
+  if [ "${success}" != "true" ]; then
+    echo "セキュリティ制御の一覧取得に失敗しました。"
+    exit 1
+  fi
+  SANI_JSON_FILE=`cat ${EXISTING_SANITIZERS_JSON}`
+  SANI_JSON_LENGTH=`echo ${SANI_JSON_FILE} | jq '.controls | length'`
+  if [ ${SANI_JSON_LENGTH} -gt 0 ]; then
+    for i in `seq 0 $(expr ${SANI_JSON_LENGTH} - 1)`; do
+      id=`echo ${SANI_JSON_FILE} | jq -r .controls[${i}].id`
+      name=`echo ${SANI_JSON_FILE} | jq -r .controls[${i}].name`
+      if [ "${PREFIX}" != "" ]; then
+        if [[ "${name}" =~ ^${PREFIX}* ]]; then
+          echo "match" ${name}
+          continue
+        fi
+      fi
+      curl -X DELETE -sS \
+        ${API_URL}/${ORG_ID}/controls/${id}?expand=skip_links \
+        -H "Authorization: ${AUTHORIZATION}" \
+        -H "API-Key: ${API_KEY}" \
+        -H 'Accept: application/json' -J -o group_del.json
+    done
+  fi
+fi
+
 JSON_FILE=`cat ${INPUT_JSON}`
 JSON_LENGTH=`echo ${JSON_FILE} | jq length`
-
 for i in `seq 0 $(expr ${JSON_LENGTH} - 1)`; do
   name=`echo ${JSON_FILE} | jq -r .[${i}].name`
   language=`echo ${JSON_FILE} | jq -r .[${i}].language`
