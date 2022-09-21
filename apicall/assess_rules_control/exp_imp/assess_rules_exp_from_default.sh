582f2c7afd2320c2966336cc150e3a8a7eecc4b8
turbou <turbou@i.softbank.jp>
2022-09-22 02:01:19 +0900

    update.

--- apicall/assess_rules_control/exp_imp/assess_rules_exp_from_default.sh
+++ apicall/assess_rules_control/exp_imp/assess_rules_exp_from_default.sh
@@ -3,10 +3,10 @@
 if [ -z "$CONTRAST_BASEURL" -o -z "$CONTRAST_API_KEY" -o -z "$CONTRAST_USERNAME" -o -z "$CONTRAST_SERVICE_KEY" -o -z "$CONTRAST_ORG_ID" ]; then
     echo '環境変数が設定されていません。'
     echo 'CONTRAST_BASEURL       : https://(app|eval).contrastsecurity.com/Contrast'
-    echo 'CONTRAST_API_KEY       : XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
     echo 'CONTRAST_USERNAME      : XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
     echo 'CONTRAST_SERVICE_KEY   : XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
     echo 'CONTRAST_ORG_ID        : XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX'
+    echo 'CONTRAST_API_KEY       : XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
     exit 1
 fi
 
@@ -16,11 +16,29 @@ USERNAME=$CONTRAST_USERNAME
 SERVICE_KEY=$CONTRAST_SERVICE_KEY
 AUTHORIZATION=`echo "$(echo -n $USERNAME:$SERVICE_KEY | base64)"`
 ORG_ID=$CONTRAST_ORG_ID
-API_URL="${BASEURL}/api/ng/${ORG_ID}"
+API_URL="${BASEURL}/api/ng/"
+
+rm -f ./organization.json
+curl -X GET -sS \
+     ${API_URL}/profile/organizations/${ORG_ID}?expand=freemium,skip_links \
+     -H "Authorization: ${AUTHORIZATION}" \
+     -H "API-Key: ${API_KEY}" \
+     -H 'Accept: application/json' -J -o organization.json
+
+ORG_NAME=`cat ./organization.json | jq -r '.organization.name'`
+if [ -z $ORG_NAME ]; then
+    echo ""
+    echo "  APIの実行に失敗しました。環境変数の値が正しいか、ご確認ください。"
+    echo ""
+    exit 1
+fi
+echo ""
+echo "  対象組織: $ORG_NAME"
+echo ""
 
 rm -f ./default_rules.json
 curl -X GET -sS \
-     ${API_URL}/rules?expand=references,customization,skip_links\&limit=1000\&offset=0\&quickFilter=ALL\&sort=title \
+     ${API_URL}/${ORG_ID}/rules?expand=references,customization,skip_links\&limit=1000\&offset=0\&quickFilter=ALL\&sort=title \
      -H "Authorization: ${AUTHORIZATION}" \
      -H "API-Key: ${API_KEY}" \
      -H 'Accept: application/json' -J -o default_rules.json
