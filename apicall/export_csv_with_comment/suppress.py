17d88896b0f7a7a522984a82569a6e9f44f95ff9
turbou <turbou@i.softbank.jp>
2023-10-12 00:17:54 +0900

    update.

--- apicall/export_csv_with_comment/suppress.py
+++ apicall/export_csv_with_comment/suppress.py
@@ -3,11 +3,27 @@ import json
 import html
 import requests
 import csv
+import argparse
 from datetime import datetime as dt
 
 def main():
+    parser = argparse.ArgumentParser(
+        prog='suppress.py', # プログラム名
+        usage='Demonstration of argparser', # プログラムの利用方法
+        description='description', # 引数のヘルプの前に表示
+        epilog='end', # 引数のヘルプの後で表示
+        add_help=True, # -h/–help オプションの追加
+    )
+    parser.add_argument('--showrule', help='Protectのルール一覧を表示だけ', action='store_true')
+    parser.add_argument('--app', help='アプリケーションID', required=True)
+    parser.add_argument('--rule', help='ルールID（例：sql-injection）', required=True)
+    parser.add_argument('--startdate', help='YYYYMMDD', required=True)
+    parser.add_argument('--enddate', help='YYYYMMDD', required=True)
+    parser.add_argument('--env', help='DEVELOPMENT|QA|PRODUCTION', required=True)
+    args = parser.parse_args()
+
     env_not_found = False
-    for env_key in ['CONTRAST_BASEURL', 'CONTRAST_AUTHORIZATION', 'CONTRAST_API_KEY', 'CONTRAST_ORG_ID', 'CONTRAST_APP_ID', 'CONTRAST_ENV', 'SUPPRESS_RULE_KEY']:
+    for env_key in ['CONTRAST_BASEURL', 'CONTRAST_AUTHORIZATION', 'CONTRAST_API_KEY', 'CONTRAST_ORG_ID']:
         if not env_key in os.environ:
             print('Environment variable %s is not set' % env_key)
             env_not_found |= True
@@ -17,34 +33,39 @@ def main():
         print('CONTRAST_AUTHORIZATION             : XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX==')
         print('CONTRAST_API_KEY                   : XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
         print('CONTRAST_ORG_ID                    : XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX')
-        print('CONTRAST_APP_ID                    : XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX')
-        print('CONTRAST_ENV                       : DEVELOPMENT|QA|PRODUCTION')
-        print('SUPPRESS_RULE_KEY                  : XXXXXXXX')
         print()
         return
-
     BASEURL=os.environ['CONTRAST_BASEURL']
     API_KEY=os.environ['CONTRAST_API_KEY']
     AUTHORIZATION=os.environ['CONTRAST_AUTHORIZATION']
     ORG_ID=os.environ['CONTRAST_ORG_ID']
-    APP_ID=os.environ['CONTRAST_APP_ID']
-    ENV=os.environ['CONTRAST_ENV']
+    headers = {"Accept": "application/json", "content-type": "application/json", "API-Key": API_KEY, "Authorization": AUTHORIZATION}
     API_URL="%s/api/ng/%s" % (BASEURL, ORG_ID)
+    if args.showrule:
+        url_rules = '%s/protection/rules/rules?expand=app_protect_rules,skip_links&q=&quickFilter=ALL&sort=name' % API_URL
+        r = requests.get(url_rules, headers=headers)
+        data = r.json()
+        #print(json.dumps(data, indent=4))
+        for rule in data['rules']:
+            print('%-50s: %s' % (rule['uuid'], rule['name']))
+        return
 
-    START_DATE = '20231002000000'
-    END_DATE = '20231008235959'
+    APP_ID=args.app
+    RULE_ID = args.rule
+    ENV=args.env
+    START_DATE = '%s000000' % args.startdate
+    END_DATE = '%s235959' % args.enddate
+
     start_unix_time = int(dt.strptime(START_DATE, '%Y%m%d%H%M%S').timestamp() * 1000)
     end_unix_time = int(dt.strptime(END_DATE, '%Y%m%d%H%M%S').timestamp() * 1000)
     print(start_unix_time, end_unix_time)
-
-    headers = {"Accept": "application/json", "content-type": "application/json", "API-Key": API_KEY, "Authorization": AUTHORIZATION}
 
     # =============== 最初に攻撃一覧を取得 ===============
     # この時点でアプリ、ルール、期間でフィルタリング
     all_attacks = []
     url_attacks = '%s/attacks?expand=skip_links&limit=15&offset=%d&sort=-startTime' % (API_URL, len(all_attacks))
     payload = '{"quickFilter":"ALL","protectionRules":["%s"],"applications":["%s"],"startDate":"%d","endDate":"%d","serverEnvironments":["%s"]}' % (
-        'sql-injection', APP_ID, start_unix_time, end_unix_time, ENV
+        RULE_ID, APP_ID, start_unix_time, end_unix_time, ENV
     )
     r = requests.post(url_attacks, headers=headers, data=payload)
     data = r.json()
@@ -65,7 +86,7 @@ def main():
     while attackIncompleteFlg:
         url_attacks = '%s/attacks?expand=skip_links&limit=15&offset=%d&sort=-startTime' % (API_URL, len(all_attacks))
         payload = '{"quickFilter":"ALL","protectionRules":["%s"],"applications":["%s"],"startDate":"%d","endDate":"%d","serverEnvironments":["%s"]}' % (
-            'sql-injection', APP_ID, start_unix_time, end_unix_time, ENV
+            RULE_ID, APP_ID, start_unix_time, end_unix_time, ENV
         )
         r = requests.post(url_attacks, headers=headers, data=payload)
         data = r.json()
