bb4d7e85cf0a58d55332b9d8fd3ea63920e3bc94
turbou <turbou@i.softbank.jp>
2023-10-12 10:18:45 +0900

    update.

--- apicall/export_csv_with_comment/suppress.py
+++ apicall/export_csv_with_comment/suppress.py
@@ -64,76 +64,34 @@ def main():
     end_unix_time = int(dt.strptime(END_DATE, '%Y%m%d%H%M%S').timestamp() * 1000)
     print(start_unix_time, end_unix_time)
 
-    # =============== 最初に攻撃一覧を取得 ===============
-    # この時点でアプリ、ルール、期間でフィルタリング
-    all_attacks = []
-    url_attacks = '%s/attacks?expand=skip_links&limit=15&offset=%d&sort=-startTime' % (API_URL, len(all_attacks))
-    payload = '{"quickFilter":"ALL","protectionRules":["%s"],"applications":["%s"],"startDate":"%d","endDate":"%d","serverEnvironments":["%s"]}' % (
+    # =============== 攻撃イベント一覧を取得 ===============
+    all_attack_events = []
+    url_attackevents = '%s/rasp/events/new?expand=skip_links&limit=15&offset=%d&sort=-timestamp' % (API_URL, len(all_attack_events))
+    payload = '{"quickFilter":"ALL","rules":["%s"],"applications":["%s"],"startDate":"%d","endDate":"%d","environments":["%s"]}' % (
         RULE_ID, APP_ID, start_unix_time, end_unix_time, ENV
     )
-    r = requests.post(url_attacks, headers=headers, data=payload)
+    r = requests.post(url_attackevents, headers=headers, data=payload)
     data = r.json()
     #print(json.dumps(data, indent=4))
-    if not data['success']:
-        print('Please check AuthorizationHeader, APIKey, OrgID, TeamServerURL are relevant')
-        return
     totalCnt = data['count']
-    print(totalCnt)
-    for attack in data['attacks']:
-        app_ids = []
-        for app in attack['attacksApplication']:
-            app_ids.append(app['application']['app_id'])
-        all_attacks.append({'attack_uuid': attack['uuid'], 'app_ids': app_ids})
+    #print(totalCnt)
+    for event in data['events']:
+        print(event['event_uuid'])
+        all_attack_events.append(event['event_uuid'])
 
-    attackIncompleteFlg = False
-    attackIncompleteFlg = totalCnt > len(all_attacks)
+    attackIncompleteFlg = True
+    attackIncompleteFlg = totalCnt > len(all_attack_events)
     while attackIncompleteFlg:
-        url_attacks = '%s/attacks?expand=skip_links&limit=15&offset=%d&sort=-startTime' % (API_URL, len(all_attacks))
-        payload = '{"quickFilter":"ALL","protectionRules":["%s"],"applications":["%s"],"startDate":"%d","endDate":"%d","serverEnvironments":["%s"]}' % (
+        url_attackevents = '%s/rasp/events/new?expand=skip_links&limit=15&offset=%d&sort=-timestamp' % (API_URL, len(all_attack_events))
+        payload = '{"quickFilter":"ALL","rules":["%s"],"applications":["%s"],"startDate":"%d","endDate":"%d","environments":["%s"]}' % (
             RULE_ID, APP_ID, start_unix_time, end_unix_time, ENV
         )
-        r = requests.post(url_attacks, headers=headers, data=payload)
-        data = r.json()
-        #print(json.dumps(data, indent=4))
-        if not data['success']:
-            print('Please check AuthorizationHeader, APIKey, OrgID, TeamServerURL are relevant')
-            return
-        for attack in data['attacks']:
-            app_ids = []
-            for app in attack['attacksApplication']:
-                app_ids.append(app['application']['app_id'])
-            all_attacks.append({'attack_uuid': attack['uuid'], 'app_ids': app_ids})
-        attackIncompleteFlg = totalCnt > len(all_attacks)
-
-    print('Total(Attack): ', len(all_attacks))
-
-    # =============== 次に攻撃イベント一覧を取得 ===============
-    # 上で取得した攻撃UUIDをキーに取得しています。
-    all_attack_events = []
-    for attack in all_attacks:
-        suppress_attack_uuid = attack['attack_uuid']
-        print(suppress_attack_uuid)
-        url_attackevents = '%s/rasp/events/new?expand=skip_links&limit=1000&offset=%d&sort=-timestamp' % (API_URL, len(all_attack_events))
-        payload = '{"attackUuid":"%s","quickFilter":"ALL","startDate":"%d","endDate":"%d"}' % (suppress_attack_uuid, start_unix_time, end_unix_time)
         r = requests.post(url_attackevents, headers=headers, data=payload)
         data = r.json()
-        totalCnt = data['count']
-        print(totalCnt)
         for event in data['events']:
             print(event['event_uuid'])
             all_attack_events.append(event['event_uuid'])
-
-        attackIncompleteFlg = True
         attackIncompleteFlg = totalCnt > len(all_attack_events)
-        while attackIncompleteFlg:
-            url_attackevents = '%s/rasp/events/new?expand=skip_links&limit=1000&offset=%d&sort=-timestamp' % (API_URL, len(all_attack_events))
-            payload = '{"attackUuid":"%s","quickFilter":"ALL","startDate":"%d","endDate":"%d"}' % (suppress_attack_uuid, start_unix_time, end_unix_time)
-            r = requests.post(url_attackevents, headers=headers, data=payload)
-            data = r.json()
-            for event in data['events']:
-                print(event['event_uuid'])
-                all_attack_events.append(event['event_uuid'])
-            attackIncompleteFlg = totalCnt > len(all_attack_events)
     print('Total(AttackEvent): ', len(all_attack_events))
 
     # =============== 最後に対象の攻撃イベントに対して消去を実行 ===============
